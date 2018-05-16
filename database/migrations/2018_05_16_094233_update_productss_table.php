35b4774b8be32fef1cbafd7d8026f57407eceebb
MacBook <alonasoroka1993@gmail.com>
2018-05-16 12:55:13 +0300

    Create migration
    $ php artisan migrate

---
+++ database/migrations/2018_05_16_094233_update_productss_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateProductssTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('warehouses_id')->unsigned()->index();
+            $table->foreign('warehouses_id')->references('id')->on('warehouses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
